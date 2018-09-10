e08d6e4de62e3b7b5e85303092e58c0e83541945
Bruno <bruno@patriapampa.com.br>
2018-09-10 18:20:11 -0300

    aula 14

---
+++ comentario.js
@@ -0,0 +1,12 @@
+// comentario 1
+console.log('linha 1')
+
+/*
+ *teste
+ */
+
+console.log('linha 2')
+
+/*
+ *comentário de multiplas linhas
+ */
---
+++ variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40  //variável let não deve ser usada novamente
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50 // não consegue atribuir valor pois não da para constante
+
+console.log(c)
